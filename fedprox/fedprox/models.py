12f29fe1dbdf9eb71ba320a5e4180727428e5803
sanywright13 <axil.wright@gmail.com>
2025-05-14 16:40:57 +0000

    uSfxSxp

--- fedprox/fedprox/models.py
+++ fedprox/fedprox/models.py
@@ -362,7 +362,7 @@ def train_one_epoch_gpaf(net,trainloader, DEVICE,client_id, epochs,batch_size,ve
             batch_size = batch_size
          
             optimizer.zero_grad()
-            _,outputs,_ = net(images)
+            _,_,outputs = net(images)
             loss = criterion(outputs, labels)
             loss.backward()
             optimizer.step()

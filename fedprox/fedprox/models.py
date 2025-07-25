0bddae10ac8f2f9b1b8966cf1cf6b773a5b81c63
SANYaxil <62213016+sanywright13@users.noreply.github.com>
2025-07-25 12:48:50 +0100

    Update models.py

--- fedprox/fedprox/models.py
+++ fedprox/fedprox/models.py
@@ -394,7 +394,7 @@ def train_one_epoch_gpaf(net,trainloader, DEVICE,client_id, epochs,batch_size,gl
             optimizer.step()
             # Metrics
             #epoch_loss += loss
-            epoch_loss += loss_cls.item() * images.size(0)
+            epoch_loss += loss.item() * images.size(0)
             #
             preds = torch.argmax(outputs, dim=1)
             accuracy.update(preds, labels)

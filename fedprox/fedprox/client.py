c7d380dc0505372f30e112730a573115ee1f90a5
SANYaxil <62213016+sanywright13@users.noreply.github.com>
2025-07-25 12:42:06 +0100

    Update client.py

--- fedprox/fedprox/client.py
+++ fedprox/fedprox/client.py
@@ -161,7 +161,7 @@ class FederatedClient(fl.client.NumPyClient):
      # Training
      N_j = None
      batch_losses=train_gpaf(self.net, self.traindata, self.device, self.client_id, self.local_epochs, self.batch_size, global_prototypes, N_j)
-     loss_sq_mean = np.mean([loss**2 for loss in batch_losses])
+     loss_sq_mean = np.mean([loss.detach().cpu().item()**2 for loss in batch_losses])
      # Send leave timestamp
      self.send_status(f"{self.server_url}/leave", {
             "client_id": self.client_id,

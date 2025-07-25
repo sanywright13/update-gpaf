c332e4f9be1c7641740216a793b88e47cf838554
SANYaxil <62213016+sanywright13@users.noreply.github.com>
2025-07-25 21:01:34 +0100

    Update server.py

--- fedprox/fedprox/server.py
+++ fedprox/fedprox/server.py
@@ -78,7 +78,7 @@ class GPAFStrategy(FedAvg):
         self.min_fit_clients = min_fit_clients
         self.min_evaluate_clients = min_evaluate_clients
         self.min_available_clients = min_available_clients
-        self.server_url = "https://bac47c19001a.ngrok-free.app/heartbeat"
+        self.server_url = "https://0de90d72bac2.ngrok-free.app/heartbeat"
 
         #clusters parameters
 

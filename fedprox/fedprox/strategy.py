5f8da77d4ca1536cc6ad4695a85b9b1fc4a67d49
sanywright13 <axil.wright@gmail.com>
2025-07-31 12:19:28 +0000

    best clients selection algorithm 6

--- fedprox/fedprox/strategy.py
+++ fedprox/fedprox/strategy.py
@@ -27,7 +27,8 @@ class FedAVGWithEval(FedAvg):
         fraction_fit: float = 10,
         fraction_evaluate: float = 1.0,
         min_fit_clients: int = 2,
-        min_available_clients: int = 18,
+        min_evaluate_clients=18
+
         evaluate_metrics_aggregation_fn: Optional[MetricsAggregationFn] = None,
         **kwargs,
     ) -> None:

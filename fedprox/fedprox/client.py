b6be5d46f06445d2427a7d915157892ed7cd83aa
SANYaxil <62213016+sanywright13@users.noreply.github.com>
2025-08-01 19:19:09 +0100

    Update client.py

--- fedprox/fedprox/client.py
+++ fedprox/fedprox/client.py
@@ -63,7 +63,7 @@ class FederatedClient(fl.client.NumPyClient):
         self.num_classes=9
         self.num_clients=num_clients
         self.batch_size=batch_size
-        self.server_url = "https://1f2523a1442d.ngrok-free.app/heartbeat"
+        self.server_url = "https://081cd74c373b.ngrok-free.app/heartbeat"
 
 
         print(f"dd Batch size client side : {self.batch_size}")
